98a31e91b9268720904b4ca5d2702c4cce7abd46
Ricardo Campos <ricardompcampos@gmail.com>
2025-05-29 20:03:05 -0300

    chore: add project base files

---
+++ silent-api/src/main/java/br/com/ricardocampos/silent_guard_api/SilentGuardApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricardocampos.silent_guard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilentGuardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilentGuardApiApplication.class, args);
+	}
+
+}
---
+++ silent-api/src/test/java/br/com/ricardocampos/silent_guard_api/SilentGuardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardocampos.silent_guard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilentGuardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
