c78526fd79bd401e54ee3dba648aec3c630235ed
Ricardo Campos <ricardompcampos@gmail.com>
2025-06-29 12:16:24 -0300

    feat: upgrade tailwind css to v4

--- silent-app/vite.config.ts
+++ silent-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -22,6 +23,7 @@ export default defineConfig({
         };
       }
     },
+    tailwindcss(),
     react()
   ],
 });
