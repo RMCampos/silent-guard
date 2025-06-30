83274cbb97d1258c6079d7a5cd8f5ba70c81a8b0
Ricardo Campos <ricardompcampos@gmail.com>
2025-06-30 15:17:36 -0300

    fix: vite config file for development

--- silent-app/vite.config.ts
+++ silent-app/vite.config.ts
@@ -2,17 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 import fs from 'fs';
+import path from 'path';
+
+const isDev = process.env.NODE_ENV === 'development';
+const keyPath = path.resolve(__dirname, '../certs/silentguard-local.key');
+const certPath = path.resolve(__dirname, '../certs/silentguard-local.crt');
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: {
-      key: fs.readFileSync('../certs/silentguard-local.key'),
-      cert: fs.readFileSync('../certs/silentguard-local.crt'),
-    },
-    allowedHosts: ['silentguard-local.ricardocampos.dev.br']
+    ...(isDev && {
+      https: {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      },
+      host: 'silentguard-local.ricardocampos.dev.br',
+      allowedHosts: ['silentguard-local.ricardocampos.dev.br']
+    })
   },
   plugins: [
     {
