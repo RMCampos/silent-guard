87c8c925730833c8836006e1f52dca26e7c80505
Ricardo Campos <ricardompcampos@gmail.com>
2025-06-30 12:00:03 -0300

    feat: enable local dev to run with certificates

--- silent-app/vite.config.ts
+++ silent-app/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./certs/localhost.key'),
+      cert: fs.readFileSync('./certs/localhost.crt'),
+    },
+    allowedHosts: ['silentguard-local.ricardocampos.dev.br']
+  },
   plugins: [
     {
       name: 'build-html',
